056800b2171a790a83a0d5ac786bcf660b724f6d
lvfeng02 <lvfeng02@baidu.com>
2024-03-15 21:47:33 +0800

    modiefied configs

--- run.sh
+++ run.sh
@@ -1,5 +1,3 @@
 
-# sh tools/dist_train.sh projects/configs/co_dino/co_dino_5scale_vitcomer_16e_o365tococo.py work_dirs/vit_comer_codetr_r50
-
 
 sh tools/dist_train.sh projects/configs/co_dino/co_dino_5scale_vitcomer_sfp_16e_o365tococo.py work_dirs/vit_comer_sfp
--- test.sh
+++ test.sh
@@ -1,4 +1,3 @@
-#sh tools/dist_test.sh projects/configs/co_dino/co_dino_5scale_vitcomer_16e_o365tococo.py work_dirs/co_dino_5scale_vitcomer_16e_o365tococo/epoch_1.pth 8 --eval bbox
 
 sh tools/dist_test.sh projects/configs/co_dino/co_dino_5scale_vitcomer_16e_o365tococo_test.py work_dirs/co_dino_5scale_vitcomer_16e_o365tococo/epoch_13.pth 8 --eval bbox
 
